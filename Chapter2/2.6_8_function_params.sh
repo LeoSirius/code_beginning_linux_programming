8f13bea3a6177e66a697a61fd9accfd4e44974d8
LeoSirius <is.li.xiaoyu@qq.com>
2020-03-31 21:56:16 +0800

    update

---
+++ Chapter2/2.6_8_function_params.sh
@@ -0,0 +1,41 @@
+#/bin/bash
+
+# 函数内部的$开头的参数变量是local的，不同于整个脚本的参数变量
+
+yes_or_no () {
+    echo "Is your name $* ?"  # 这里已$*其实就是整个脚本的$1参数
+    while true
+    do
+        echo -n "Enter yes or no: "
+        read x
+        case "$x" in
+            y | yes ) return 0;;
+            n | no  ) return 1;;
+            *       ) echo "Answer yes or no"
+        esac
+    done
+}
+
+echo "Original parameters are $*"
+
+if yes_or_no "$1"
+then
+    echo "Hi $1, nice name"
+else
+    echo "Never mind"
+fi
+exit 0
+
+
+# leo@ubuntu:~/c_test$ ./t.sh leo sirius
+# Original parameters are leo sirius
+# Is your name leo ?
+# Enter yes or no: e
+# Answer yes or no
+# Enter yes or no: n
+# Never mind
+# leo@ubuntu:~/c_test$ ./t.sh leo sirius
+# Original parameters are leo sirius
+# Is your name leo ?
+# Enter yes or no: y
+# Hi leo, nice name
