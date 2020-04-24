4eadb7d50d7a25271c98f153407ef66cb4be9b29
LeoSirius <is.li.xiaoyu@qq.com>
2020-04-24 16:15:15 +0800

    update

--- Chapter5_终端/5.4_termios.c
+++ Chapter5_终端/5.4_termios.c
@@ -11,20 +11,21 @@
 
 int main()
 {
-    struct termios initialrsettings, newrsettings;
+    struct termios init_settings, new_settings;
     char password[PASSWORD_LEN + 1];
 
-    tcgetattr(fileno(stdin), &initialrsettings);
-    newrsettings = initialrsettings;
-    newrsettings.c_lflag &= ~ECHO;    /* 关闭ECHO标志 */
+    tcgetattr(fileno(stdin), &init_settings);
+    new_settings = init_settings;
+    new_settings.c_lflag &= ~ECHO;    /* 关闭ECHO标志 */
 
     printf("Enter password: ");
 
-    if (tcsetattr(fileno(stdin), TCSAFLUSH, &newrsettings) != 0) {
+    if (tcsetattr(fileno(stdin), TCSAFLUSH, &new_settings) != 0) {
         fprintf(stderr, "Could not set attributes\n");
     }
+
     fgets(password, PASSWORD_LEN, stdin);
-    tcsetattr(fileno(stdin), PASSWORD_LEN, &initialrsettings);
+    tcsetattr(fileno(stdin), PASSWORD_LEN, &init_settings);
     fprintf(stdout, "\nYou entered %s\n", password);
 
     exit(0);
